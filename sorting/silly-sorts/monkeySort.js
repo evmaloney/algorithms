8a350c7f3c1492b1f408ef81296c13e2934400af
Evan Maloney <evanpmaloney@gmail.com>
2024-01-19 01:55:38 -0800

    Complete Monkey Sort

--- sorting/silly-sorts/monkeySort.js
+++ sorting/silly-sorts/monkeySort.js
@@ -6,29 +6,32 @@
 // It is often referred to as the worst sorting algorithm known to
 // human, ...but the best known to monkey.
 
-unsortedArr = [4, 8, 2, 1, 10, 9, 7, 3, 6, 5];
-let iters = 1;
+unsortedArr = [3, 4, 2, 1];
 
 const monkeySort = (arr) => {
-  if (iters === 3) return;
-  let loop = 1;
 
+  // Fisher-Yates style shuffle
   for (let i = 0; i < arr.length; i++) {
     const swapIndex = Math.floor(Math.random() * arr.length);
     [arr[i], arr[swapIndex]] = [arr[swapIndex], arr[i]];
-    console.log(`iters: ${iters} | loops: ${loop}`, arr)
-    loop++;
   }
-  console.log(`iters: ${iters} | loops: ${loop}`, arr)
-  iters++
-  // let sorted = true;
-  // let i = 0;
-  // while (sorted) {
-  //   if (i === arr.length - 1) return arr;
-  //   else if (arr[i] > arr[i + 1]) sorted = false;
-  // }
-  monkeySort(arr)
+
+  let sorted = true;
+  let i = 0;
+  // Iterate through the array to check if sorted
+  while (sorted) {
+    // Base case -> if we've made it to the last element
+    // then array is sorted.
+    if (i === arr.length - 1) {
+      return arr;
+    }
+    else if (arr[i] > arr[i + 1]) sorted = false;
+    i++;
+  }
+
+  // Use recursion to call function with most recent version of array
+  return monkeySort(arr);
 };
 
 const sorted = monkeySort(unsortedArr);
-// console.log(sorted);
+console.log(sorted);
