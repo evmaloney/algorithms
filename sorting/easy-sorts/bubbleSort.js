dc1ad239551e8798b50b8c523c65de15d51234b1
Evan Maloney <evanpmaloney@gmail.com>
2024-02-17 19:17:41 -0800

    Bubble Sort done

--- sorting/easy-sorts/bubbleSort.js
+++ sorting/easy-sorts/bubbleSort.js
@@ -5,22 +5,25 @@
 
 unsortedArr = [4, 8, 2, 1, 10, 9, 7, 3, 6, 5];
 
-// Bubble Sort
+// Bubble Sort Basic
 const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
+    console.log(i)
   }
+
   return arr;
 }
 
-// const sortedBubble = bubbleSort(unsortedArr);
-// console.log(sortedBubble);
+const sortedBubble = bubbleSort(unsortedArr);
+console.log(sortedBubble);
+
 
 // Sink Sort (opposite of bubbling right - slightly optimized)
 const sinkSort = (arr) => {
@@ -30,20 +33,45 @@ const sinkSort = (arr) => {
         [arr[i], arr[j]] = [arr[j], arr[i]];
       }
     }
+    console.log(i)
   }
   return arr;
 };
 
-// const sortedSink = sinkSort(unsortedArr);
-// console.log(sortedSink);
+const sortedSink = sinkSort(unsortedArr);
+console.log(sortedSink);
+
+
+// Bubble Sort Optimized Version
+unsortedArrOptimized = [3, 2, 1, 4, 5, 6, 7, 8, 9, 10];
+
+const bubbleSortOptimized = (arr) => {
+  let swapped = true;
+  let i = 0;
+
+  while(swapped) {
+    swapped = false;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+    i++;
+  }
+  console.log(i)
+  return arr;
+};
+
+const sortedBubbleOpt = bubbleSortOptimized(unsortedArr);
+console.log(sortedBubbleOpt);
 
 
 // Sink Sort Optimized Version
-unsortedArrOptimized = [3, 2, 1, 4, 5, 6, 7, 8, 9, 10];
-
 const sinkSortOptimized = (arr) => {
   let swapped = true;
   let i = 0;
+
   while (swapped) {
     swapped = false;
     for (let j = i + 1; j < arr.length; j++) {
@@ -54,8 +82,9 @@ const sinkSortOptimized = (arr) => {
     }
     i++;
   }
+  console.log(i)
   return arr;
 };
 
 const sortedSinkOpt = sinkSortOptimized(unsortedArrOptimized);
-console.log(sortedSinkOptimized);
+console.log(sortedSinkOpt);
